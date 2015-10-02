611456c60a1ee2a6f717cf39534de6678f5e5ce1
Kazuya <IgYmB0218LP@gmail.com>
2015-10-02 14:55:34 +0900

    add polymorphic

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,7 @@ class Question < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
   has_many   :answers, dependent: :destroy
+  has_one    :feed_content, as: :content, dependent: :destroy
 
   #validation
   validates_presence_of :user_id, :text, :group_id
