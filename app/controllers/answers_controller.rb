e87c3e0965fabc578075226b61f765cea6c1d003
Kazuya <IgYmB0218LP@gmail.com>
2015-10-02 14:49:09 +0900

    modify answer2

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,10 +5,24 @@ class AnswersController < ApplicationController
     @question = Question.find(params[:question_id])
     @answer = Answer.new
     @answer.question_id = @question.id
+    if @question.answers.exists?(user_id: current_user.id)
+      redirect_to :root
+    end
   end
 
   def create
     @answer = Answer.create(create_params)
+  end
+
+  def edit
+    @answer = Answer.find(params[:id])
+    @question = @answer.question
+  end
+
+  def update
+    @answer = Answer.find(params[:id])
+    @question = @answer.question
+    @answer.update(update_params)
   end
 
   private
@@ -20,6 +34,10 @@ class AnswersController < ApplicationController
     if Answer.exists?(question_id: params[:question_id], user_id: current_user.id)
       redirect_to :root
     end
+  end
+
+  def update_params
+    params.require(:answer).permit(:text)
   end
 
 end
